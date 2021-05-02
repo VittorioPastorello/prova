0cbe77b88c0764dd3591c155237db4334f0dc609
VittorioPastorello <PSTVTR01R24B429L@studium.unict.it>
2021-05-02 13:35:04 +0200

    aggiunto nuovo file

---
+++ mainveicolo.cpp
@@ -0,0 +1,11 @@
+#include "moto.hpp"
+#include "auto.hpp"
+
+
+int main(){
+ moto m (120, 10000, "ducati", "1000cc");
+ auto a (160, 8000, "fiat punto","diesel","1300 multijet diesel");
+
+ cout<<m<<endl;
+ cout<<a<<endl;
+}
