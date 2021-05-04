3a30ddd65dd50ac677fd8e468e396c299cbd4491
VittorioPastorello <PSTVTR01R24B429L@studium.unict.it>
2021-05-04 23:43:14 +0200

    aggiornato con classifica finale

--- mainveicolo.cpp
+++ mainveicolo.cpp
@@ -1,11 +1,33 @@
 #include "moto.hpp"
 #include "auto.hpp"
-
+#include "gara.hpp"
 
 int main(){
- moto m (120, 10000, "ducati", "1000cc");
- auto a (160, 8000, "fiat punto","diesel","1300 multijet diesel");
+	
+/*	double *array;
+	array=new double [10];
+for(int i=0; i<10;i++){
+	cout<<array[i]<<" ";
+}*/
 
+cout<<endl;//questo stampera qualcosa perchè abbiamo una libreria definita per questa
+//la classe astratta presenterà dei problemi, vedere array di veicoli in GARA
+//utilizziamo array di puntatori
+ moto m (120, 10000, "ducati", "1000cc");
+ Auto a (160, 8000, "fiat punto","diesel","1300cc MTJ");
+ Auto b (400, 20000, "ferrari gtx","diesel","5000cc");
+ Auto c (400, 20000, "speed","diesel","2500cc");
+  Auto u (400, 20000, "ferrari gtx","diesel","5000cc");
  cout<<m<<endl;
  cout<<a<<endl;
+ cout<<b<<endl;
+ 
+ gara Gara(10, 10);
+	Gara.aggiungipartecipante(m);
+	Gara.aggiungipartecipante(a);
+	Gara.aggiungipartecipante(b);
+	Gara.aggiungipartecipante(c);
+	Gara.aggiungipartecipante(u);
+	Gara.partenza();
+
 }
